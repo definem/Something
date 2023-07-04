199e16c3a2abe3d2479a6f28315b3bbc35c79256
define) <120991965+definem@users.noreply.github.com>
2023-07-04 11:19:32 +0300

    updated

--- script.js
+++ script.js
@@ -5,45 +5,44 @@ const inpTitle = document.getElementById("title");
 
 let titles = [];
 
-form.addEventListener("submit", (evt) => {
-  evt.preventDefault();
-  titles.unshift(inpTitle.value);
-  renderToWrapper();
-console.log(titles);
-  inpTitle.value = "";
-});
-
 function renderToWrapper() {
   let res = "";
-  titles.forEach((element) => {
+  titles.forEach((element, index) => {
     res += `<div class="card mt-2">
-    <div class="card-body" style="font-weight: 600">${element}</div>
+    <div class="card-body" style="font-weight: 600">${element.title}</div>
     <div class="card-footer">
-      <button class="btn btn-danger">Delete</button>
-      <button class="btn btn-warning">Edit</button>
+      <button class="btn btn-danger" onclick="deleteTitle(${index})">Delete</button>
+      <button class="btn btn-warning" onclick="editTitle(${index})">Edit</button>
       <button class="btn btn-info">Copy</button>
     </div>
   </div>`;
   });
 
   wrapper.innerHTML = res;
-};
+}
 
-form.addEventListener('submit', (e) => {
+form.addEventListener("submit", (e) => {
   e.preventDefault();
-  let obj = {id: Date.now(), title: inpTitle.value 
-};
 
-inpTitle.value === "" ? alert("Enter something to input: ") : titles.push(obj);
+  let obj = { id: Date.now(), title: inpTitle.value };
+
+  if (inpTitle.value.length < 1) {
+    alert("Enter something to input: ");
+  } else {
+    titles.push(obj);
+    inpTitle.value = "";
+    renderToWrapper();
+  }
+});
 
 function deleteTitle(ind) {
   titles.splice(ind, 1);
   renderToWrapper();
-};
+}
 
 function editTitle(ind) {
-  titles[ind].title = promptTitle("Enter new title");
+  titles[ind].title = prompt("Enter new title");
   renderToWrapper();
-};
+}
 
 renderToWrapper();
